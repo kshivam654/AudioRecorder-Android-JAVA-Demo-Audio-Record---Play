559432097194ccdf35b72526e2adda84645855ca
Shivam Sharma <kshivam654@gmail.com>
2021-02-06 11:49:45 +0530

    first commit

---
+++ app/src/main/java/com/sharma/audiorecorder/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sharma.audiorecorder.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sharma.audiorecorder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
